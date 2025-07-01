0e5759f5864a82eef29269dd04cd0305386b08be
LucaCampisi27 <lucacampisi@live.com.ar>
2025-06-30 23:49:42 -0300

    Configuracion

--- src/App.js
+++ src/App.js
@@ -457,19 +457,16 @@ const App = () => {
             {/* El texto "CEA" se ha eliminado del span si solo quieres la imagen */}
           </div>
           <ul className="footer-links">
-            <li><a href="javascript:void(0)" className="footer-link">Privacidad</a></li>
-<li><a href="javascript:void(0)" className="footer-link">Términos</a></li>
-<li><a href="javascript:void(0)" className="footer-link">Mapa del sitio</a></li>
-          </ul>
-          <div className="social-icons">
-            {/* Iconos de redes sociales (placeholders) */}
-            <a href="#" className="social-icon-link">
-              <svg className="social-icon" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
-                <path fillRule="evenodd" d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33V22C18.343 21.128 22 16.991 22 12z" clipRule="evenodd" />
-              </svg>
-            </a>
-            
-          </div>
+            <li><button className="footer-link" onClick={() => alert("Privacidad")}>Privacidad</button></li>
+<li><button className="footer-link" onClick={() => alert("Términos")}>Términos</button></li>
+<li><button className="footer-link" onClick={() => alert("Mapa del sitio")}>Mapa del sitio</button></li>
+</ul>
+
+<div className="social-icons">
+  {/* Iconos de redes sociales (placeholders) */}
+  <button className="social-icon-link" onClick={() => alert("Red social")}>🌐</button>
+</div>
+
           <p className="copyright-text">
             &copy; {new Date().getFullYear()} CEA Logística. Todos los derechos reservados.
           </p>
